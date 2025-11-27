0b9c92cb8fdd99cb6c0a9f4e773947d2b8aff660
Eero Seppänen <ejs.seppanen@gmail.com>
2025-11-27 20:21:29 +0200

    konflikti ratkaistu

--- print.cpp
+++ print.cpp
@@ -3,9 +3,6 @@
 
 void printHelloWorld() {
     std::cout << "Hello, World!" << std::endl;
-<<<<<<< HEAD
     std::cout << "Hei, maailma!" << std::endl;
-=======
     std::cout << "Hej, världen!" << std::endl;
->>>>>>> ruotsi
 }
